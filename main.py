02337712e5133b3f8028f1bbdb7bf647b841a5d9
Jorge Antonio Umaña ortega <umana3750@gmail.com>
2026-06-18 18:56:42 -0600

    Estudiante A: Agrega funcion calcular_area_cuadrado

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-# main.py - Calculadora Geométrica en equipo modo COLABORATIVO
+# main.py - Calculadora Geométrica en equipo
+def calcular_area_cuadrado(lado):
+ return lado * lado
 print("--- BIENVENIDO A LA CALCULADORA GEOMÉTRICA ---")
+print(f"Área de un cuadrado de lado 5: {calcular_area_cuadrado(5)}")
